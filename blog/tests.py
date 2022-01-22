a0cb3036f4017caeeb2ef3f2cc95b4651b6336ae
Trianglium <jbrockerrudow@gmail.com>
2022-01-22 17:32:10 +0000

    Post TestCases

--- blog/tests.py
+++ blog/tests.py
@@ -2,6 +2,16 @@ from django.test import TestCase
 from blog.models import Post, Comment
 from django.contrib.auth.models import User
 
+class PostTestCase(TestCase):
+  flds = ["Newest Post","Just a Test","Will p2 be able to pass the test? lets find out...","test"]
+  p=Post.objects.first()
+  p2=Post(author=self.p.author, title="Newest Post", summary="Just a Test", content="Will p2 be able to pass the test? lets find out...", tags="test")
+
+  def test_new_post_content_type(self):
+    self.assertEqual(self.p2.content_type, self.p.content_type)
+
+  def test_new_post_title(self):
+    self.assertEqual(self.p2.title, self.flds[0])
 
 class PostCommentTestCase(TestCase):
   p=Post.objects.first()
