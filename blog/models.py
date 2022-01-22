03dfe2610a1268ac50905975d4acb768fad57ca1
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-22 16:41:43 +0000

    Comment Model & Generic Relationships

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+class Comment(models.Model):
+    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
 
 class Tag(models.Model):
     value = models.TextField(max_length=100)
