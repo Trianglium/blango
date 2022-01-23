00e928a61ec52673b957f8ac9bf81a5c5f31590c
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-23 07:58:33 -0600

    Add blog_extras.py for Author functions

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,16 @@
+from django.contrib.auth import get_user_model
+  
+user_model = get_user_model()
+
+def author_details(author):
+    if not isinstance(author, user_model):
+    # return empty string as safe default 
+        return ""
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+
+    else:
+        name = f"{author.username}"
+    
+    return name
