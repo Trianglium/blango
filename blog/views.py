c5eec19647020dc897c7d3f5e36070d8d425eb4c
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-23 13:57:05 -0600

    Update views.py - add details view

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 
 def index(request): 
     posts = Post.objects.filter(published_at__lte=timezone.now()) 
     return render(request, "blog/index.html", {"posts": posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post":post})
