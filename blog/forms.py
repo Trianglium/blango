41e37a0df42c135610cbb45ecb10ede70fd60e14
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-24 10:14:22 -0600

    add template tag and form helper class

--- blog/forms.py
+++ blog/forms.py
@@ -1,5 +1,8 @@
 from django import forms 
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+
 from blog.models import Comment
 
 
@@ -7,3 +10,8 @@ class CommentForm(forms.ModelForm):
   class Meta:
     model = Comment 
     fields = ["content"]
+
+  def __init__(self, *args, **kwargs): 
+    super(CommentForm, self).__init__(*args, **kwargs) 
+    self.helper = FormHelper()
+    self.helper.add_input(Submit('submit', 'Submit'))
