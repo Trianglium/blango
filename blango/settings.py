c3467978ecbd7be92d57fecdba6979c2b492f22f
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-26 16:16:05 +0000

    Update Logging Config Settings, Verbose Formatter Added

--- blango/settings.py
+++ blango/settings.py
@@ -29,8 +29,19 @@ class Dev(Configuration):
   LOGGING = {
       "version": 1,
       "disable_existing_loggers": False,
+      "formatters": {
+          "verbose": {
+              "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+              # Outputs "INFO 2022-01-26 16:04:49,067 views 462 140309436626688 Created comment on Post 2 for user trianglium"
+              "style": "{",
+          },
+      },
       "handlers": {
-          "console": {"class": "logging.StreamHandler", "stream": "ext://sys.stdout"},
+          "console": {
+              "class": "logging.StreamHandler", 
+              "stream": "ext://sys.stdout",
+              "formatter": "verbose",
+          },
       },
       "root": {
           "handlers": ["console"],
