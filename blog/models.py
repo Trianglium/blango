ce450c2e8ce04a6688d14df064160009f9cbc806
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-28 15:08:28 +0000

    Author Profile Section and Model

--- blog/models.py
+++ blog/models.py
@@ -36,3 +36,14 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+# AuthorProfile as to separate info from the User Auth Model
+# Allowing for a separation of concerns
+# And added security.
+class AuthorProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
+    bio = models.TextField()
+
+    def __str__(self):
+        return f"{self.__class__.__name__} object for {self.user}"
