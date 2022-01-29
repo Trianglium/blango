59afa00b09acb653527d516d9da69935ed63314f
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-29 16:16:03 +0000

    Registration Forms and 2 Factor Auth

--- blango/urls.py
+++ blango/urls.py
@@ -1,25 +1,15 @@
-"""blango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 import debug_toolbar
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
+from django_registration.backends.activation.views import RegistrationView
+
 import blog.views
+
 import blango_auth.views
+import blango_auth.forms import BlangoRegistrationForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,6 +17,11 @@ urlpatterns = [
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
     path('accounts/', include('django_registration.backends.activation.urls')),
+    path(
+        "accounts/register/",
+        RegistrationView.as_view(form_class=BlangoRegistrationForm),
+        name="django_registration_register",
+    ),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
 ]
 
