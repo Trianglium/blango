99971d8becb3943460c86c12b45100dda25a0876
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-01-30 15:08:42 +0000

    Two-step Activation Complete

--- blango/urls.py
+++ blango/urls.py
@@ -3,11 +3,10 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
-from django_registration.backends.activation.views import RegistrationView
-
 import blog.views
 
 import blango_auth.views
+from django_registration.backends.activation.views import RegistrationView
 from blango_auth.forms import BlangoRegistrationForm
 
 
@@ -16,13 +15,14 @@ urlpatterns = [
     path('', blog.views.index),
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
-    path('accounts/', include('django_registration.backends.activation.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
     path(
-        "accounts/register/",
-        RegistrationView.as_view(form_class=BlangoRegistrationForm),
-        name="django_registration_register",
+    "accounts/register/",
+    RegistrationView.as_view(form_class=BlangoRegistrationForm),
+    name="django_registration_register",
     ),
     path("accounts/profile/", blango_auth.views.profile, name="profile"),
+    path("accounts/", include("django_registration.backends.activation.urls")),
 ]
 
 # Map the path __debug__/ to the DJDT's URL's, but only in debug mode.
