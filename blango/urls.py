5b83ed2393c93f1b5c77d0dbdb2cbd82909f779f
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-01 09:42:51 -0600

    Add Allauth to Urls
    Path was put after django.contrib.auth.urls so that django could have precedence.

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path("post/<slug>/", blog.views.post_detail, name="blog-post-detail"),
     path("ip/", blog.views.get_ip),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("allauth.urls")),
     path(
     "accounts/register/",
     RegistrationView.as_view(form_class=BlangoRegistrationForm),
