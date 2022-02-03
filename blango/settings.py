3f43c0f55b7d41fc973cf5467b9c69b1737df7fe
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-03 12:59:34 +0000

    Setut rest framework for future functionality

--- blango/settings.py
+++ blango/settings.py
@@ -129,6 +129,7 @@ class Dev(Configuration):
       'allauth.account',
       'allauth.socialaccount',
       'allauth.socialaccount.providers.google',
+      'rest_framework',
   ]
 
   MIDDLEWARE = [
