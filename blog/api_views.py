0de312a47439453109143b6472d671560c2883eb
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-04 12:37:40 +0000

    Refining class based API views

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,46 +1,14 @@
-from http import HTTPStatus
-
-from django.urls import reverse
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework import mixins
 from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
 
-class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class PostDetail(APIView):
-    @staticmethod
-    def get_post(pk):
-        return get_object_or_404(Post, pk=pk)
-
-    def get(self, request, pk, format=None):
-        post = self.get_post(pk)
-        return Response(PostSerializer(post).data)
-
-    def put(self, request, pk, format=None):
-        post = self.get_post(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=HTTPStatus.NO_CONTENT)
-        return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        post = self.get_post(pk)
-        post.delete()
-        return Response(status=HTTPStatus.NO_CONTENT)
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
