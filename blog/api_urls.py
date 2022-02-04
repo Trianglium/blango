06e3777ddacf67bfee55d086d61519ab39edef5a
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-04 12:16:30 +0000

    DRF Changes to URL patterns

--- blog/api_urls.py
+++ blog/api_urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
 from blog.api_views import post_list, post_detail
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path("posts/", post_list, name="api_post_list"),
-    path("posts/<int:pk>/", post_detail, name="api_post_detail"),
+    path("posts/<int:pk>", post_detail, name="api_post_detail"),
 ]
+
+# Enables JSON data for a Post with a url like 'http://*website*/api/v1/3.json'
+# even from a browser.
+# It is passed a list of URL patterns, and adds extra patterns with a suffix. 
+urlpatterns = format_suffix_patterns(urlpatterns)
