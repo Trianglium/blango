4a9b0a45350343dcd695340de028600dcbab120d
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-04 12:31:54 +0000

    PostList DRF Mixins & Generic

--- blog/api_views.py
+++ blog/api_views.py
@@ -1,31 +1,28 @@
 from http import HTTPStatus
 
 from django.urls import reverse
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import mixins
+from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
 
-@api_view(["GET", "POST"])
-def post_list(request, format=None):
-    if request.method == "GET":
-        posts = Post.objects.all()
-        return Response({"data": PostSerializer(posts, many=True).data})
-    elif request.method == "POST":
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            post = serializer.save()
-            return Response(
-                status=HTTPStatus.CREATED,
-                headers={"Location": reverse("api_post_detail", args=(post.pk,))},
-            )
-        return Response(serializer.errors, status=HTTPStatus.BAD_REQUEST)
+class PostList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-@api_view(["GET", "PUT", "DELETE"])
 class PostDetail(APIView):
     @staticmethod
     def get_post(pk):
