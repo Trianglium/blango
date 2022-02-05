52662a6d2bc2539b2522044bc4ed4d63899c0a93
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-05 14:02:01 +0000

    Authenticating with the Session - DRF

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 
 from blog.api.views import PostList, PostDetail
@@ -8,4 +8,8 @@ urlpatterns = [
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
 ]
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns = format_suffix_patterns(urlpatte
+# DRF login url
+urlpatterns += [
+    path("auth/", include("rest_framework.urls")),
+]
