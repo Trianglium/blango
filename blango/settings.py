8f1acc1ab6285fe1d979d63f54f14e0c0a7f1d74
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-06 08:37:47 -0600

    Adding Permissions to Views - Configuration

--- blango/settings.py
+++ blango/settings.py
@@ -256,7 +256,10 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-    ]
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+      "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ],
 }
 
 # Production 
