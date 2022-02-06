8743a42d923067803d17c84b3bee1179b6dbb7e0
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-06 08:41:50 -0600

    Update PostDetail Permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,10 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from blog.api.serializers import PostSerializer
 from blog.models import Post
+from blog.api.permissions import AuthorModifyOrReadOnly
+
 
 
 class PostList(generics.ListCreateAPIView):
@@ -10,5 +13,6 @@ class PostList(generics.ListCreateAPIView):
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [AuthorModifyOrReadOnly]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
