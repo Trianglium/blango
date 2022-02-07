b5b55a5654bbe8276c144d8b0d8af0847e7dcc26
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-07 18:09:37 +0000

    Finish authentication

--- blog/api/tests.py
+++ blog/api/tests.py
@@ -1,9 +1,24 @@
 from django.test import TestCase
 
-class APITestCase(TestCase):
-    base_url= ""
-    class APIAuthTestCase(APITestCase):
+import requests
+from requests.auth import HTTPBasicAuth
 
-        def test_incorrect_auth_info(self):
-            resp = requests.get(self.base_url, auth=HTTPBasicAuth("user@example.com", "badpassword"))
-            self.assertEqual(str(resp), '<Response [403]>')
+class APITestCase(TestCase):
+    base_url= "https://aladdin-heavy-8000.codio.io/api/v1/"
+
+
+class PostAPIAuthTestCase(APITestCase):
+    # Correct URL for current box
+    base_url = "https://aladdin-heavy-8000.codio.io/api/v1/"
+
+    def 
+
+    def test_incorrect_auth_info(self):
+        """ Input incorrect authentication information, recieve a 403 error response """
+        resp = requests.get(self.base_url, auth=HTTPBasicAuth("user@example.com", "badpassword"))
+        self.assertEqual(str(resp), '<Response [403]>')
+        self.assertEqual(str(resp), '<Response [403]>')
+    def test_correct_auth_info(self):
+        """ Input correct authentication information, recieve a 200 response """
+        resp = requests.get(self.base_url, auth=HTTPBasicAuth("user200@gmail.com", "G00dpassword"))
+        self.assertEqual(str(resp), '<Response [200]>') 
