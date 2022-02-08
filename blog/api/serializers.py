500055f6c6313acc1d58fda4e42abfedb494894e
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-08 13:57:35 +0000

    Customizing HyperlinkRelatedField & Email lookup

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -2,11 +2,16 @@ from rest_framework import serializers
 from blog.models import Post
 
 class PostSerializer(serializers.ModelSerializer):
+    # Tags as string objects - unqiue (They were ints previously)
+    tags = serializers.SlugRelatedField(
+        slug_field="value", many=True, queryset=Tag.objects.all()
+        )
     # serializes a related object to a URL at which we can retrieve the full detail of the object
     # Requires name of view 
-    tags = serializers.HyperlinkedRelatedField(
+    author = serializers.HyperlinkedRelatedField(
         queryset=User.objects.all(),
-        view_name="api_user_detail"
+        view_name="api_user_detail",
+        lookup_fields="email",
         )
     class Meta:
         model = Post
