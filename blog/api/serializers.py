b8447619d3c2e93bb538ff9871af78440e7335ab
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-08 14:24:40 +0000

    Nested Relationships; Serializers

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
 
-from blog.models import Post, Tag
+from blog.models import Post, Tag, Comment
 from blango_auth.models import User
 
+
+# UserSerializer must be first
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
+
+class TagField(serializers.SlugRelatedField):
+    def to_internal_value(self, data):
+        try:
+            return self.get_queryset().get_or_create(value=data.lower())[0]
+        except (TypeError, ValueError):
+            self.fail(f"Tag value {data} is invalid")
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(required=False)
+    creator = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ["id", "creator", "content", "modified_at", "created_at"]
+        readonly = ["modified_at", "created_at"]
+
+# PostSerializer must come after UserSerializer and CommentSerializer
 class PostSerializer(serializers.ModelSerializer):
     # Tags as string objects - unqiue (They were ints previously)
     tags = serializers.SlugRelatedField(
@@ -24,8 +49,22 @@ class PostSerializer(serializers.ModelSerializer):
         fields = "__all__"
         readonly = ["modified_at", "created_at"]
 
+# PostDetailSerializer Must come at the end, below User, Comment, and Post. TagSerializer's order does not matter.
+class PostDetailSerializer(PostSerializer):
+    comments = CommentSerializer(many=True)
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["first_name", "last_name", "email"]
+    def update(self, instance, validated_data):
+        comments = validated_data.pop("comments")
+
+        instance = super(PostDetailSerializer, self).update(instance, validated_data)
+
+        for comment_data in comments:
+            if comment_data.get("id"):
+                # comment has an ID so was pre-existing
+                continue
+            comment = Comment(**comment_data)
+            comment.creator = self.context["request"].user
+            comment.content_object = instance
+            comment.save()
+
+        return instance
--- blog/api/views.py
+++ blog/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from rest_framework.authentication import SessionAuthentication
 
-from blog.api.serializers import PostSerializer, UserSerializer
+from blog.api.serializers import PostSerializer, UserSerializer, PostDetailSerializer
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 
 from blog.models import Post
@@ -17,7 +17,7 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    serializer_class = PostDetailSerializer
 
 class UserDetail(generics.RetrieveAPIView):
     lookup_field = "email"
