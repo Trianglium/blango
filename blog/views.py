494720451ef6efc8343b14c2d21173bc696e8d57
Trianglium <jbrockerrudow@gmail.com>
2022-02-20 08:01:38 -0600

    Function to render js template

--- blog/views.py
+++ blog/views.py
@@ -54,3 +54,7 @@ def post_detail(request, slug):
         comment_form = None
 
     return render(request, "blog/post-detail.html", {"post": post, "comment_form": comment_form})
+
+
+def post_table(request):
+    return render(request, "blog/post-table.html")
