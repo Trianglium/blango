89a2b2d3aead6fc1834de46773ffb6254243659a
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-20 14:24:41 +0000

    Variables in JS: Strings

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,1 +1,6 @@
-alert('Hello, world!')
+const framework = 'Django'
+const language = 'Python'
+alert(framework + ' is written in ' + language)
+
+  /* Note - Lines in javascript can be terminated with a semi colon. concatenated works the same as it does in python.
+   */ 
