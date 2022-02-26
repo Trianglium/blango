a93fd9db4899c883329ac2b10c8b92a3767cf6e2
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-26 14:14:30 +0000

    BS Pagination - Notes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -156,3 +156,38 @@ There are also a few other lifecycle methods that are called, which are rarely u
 
 https://reactjs.org/docs/react-component.html#rarely-used-lifecycle-methods
  */
+
+// Other Notes - Pagination 
+/*
+https://getbootstrap.com/docs/5.1/components/pagination/
+
+Further Enhancements
+
+Right now our React post table doesn’t have any advantages over just building a table in plain HTML. The next features to add would be sorting of results by clicking on the table headers, as well as going through the results page by page. Since we have a paginated response, we’re only actually showing the first 100 posts in the table.
+We won’t be covering in detail how to make these changes, as you already have the knowledge to do it if you’re interested. But here’s a brief overview of a suggested method:
+Create a “pagination” component that renders a previous button, next button, and a list of pages numbers. Using a Bootstrap Pagination Component would help.
+Use the next, previous and count values of the Post list response to control how the pagination component is rendered. The previous and next buttons would only be shown if the values are not null in the response.
+Add onClick handler to the table headers to set the result ordering.
+Create a wrapper component that contains the PostTable and new pagination component. The wrapper component would do the fetch() instead of the PostTable, and then pass the relevant values to its child components.
+In order for the child components to trigger a refresh on the parent, they’ll need to call parent methods. Parent methods can actually be passed to the child components as properties. Here’s a small example, a button that when clicked executes a parent method:
+
+
+class ChildButton extends React.Component {
+  render () {
+    return <button onClick={ () => { this.props.parentCallback('foo') } }>Click Me</button>
+  }
+}
+
+class ParentContainer extends React.Component {
+  aCallback(val) {
+    // will log 'foo' when child is clicked
+    console.log(val)
+  }
+
+  render () {
+    return <div>
+      <ChildButton parentCallback={ (arg) => { this.aCallback(arg) } } />
+    </div>
+  }
+}
+ */
