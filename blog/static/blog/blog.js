b033ea75558f4aa6b00c99e23ca2856821841619
Jordan Brocker Rudow <jbrockerrudow@gmail.com>
2022-02-26 13:47:28 +0000

    Fetch Intro Notes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -86,3 +86,114 @@ ReactDOM.render(
   domContainer
 )
 
+// Fetch Intro
+/* https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+- fetch() is a function built into all modern browsers that preforms HTTP Requests. 
+
+- It takes two arguments, the URL and the second is not mandetory and contains all options for the request.
+
+Examples:
+ GET         
+          fetch('/api/v1/posts/')
+
+ POST          
+          fetch('/api/v1/posts/', {
+            method: 'POST',
+            body: data
+          })
+Since Fetch is promise based, a callback function to then 'then()' will need to be provided. It deals with the response, and is done by calling json().
+
+fetch('/api/v1/posts/').then(response => {
+  return response.json()
+})
+
+Is the JSON data now returned from the function? No, the json() method actually returns another Promise. Luckily, we can chain promises together so that our source code doesn’t get to unwieldy:
+
+
+fetch('/api/v1/posts/').then(response => {
+  return response.json()
+}).then(data  => {
+  // do something with data, for example
+  console.log(data)
+})
+
+
+
+Like in Python, JS has try / catch / finally error handling. 
+
+
+In Python - 
+
+
+try:
+    raise Exception("Something went wrong")
+except TypeError as e:
+    print("Got type error", e)
+except Exception as e:
+    print("Got Exception", e)
+finally:
+    print("This is always called")
+
+
+
+Equivalent in JavaScript -
+
+try {
+  throw new Error('Something went wrong)
+} catch(e) {
+  if (e instanceof TypeError) {
+    console.log('Got type error')
+    console.log(e)
+  } else {
+    console.log('Got Exception')
+    console.log(e)
+  }
+} finally {
+  console.log('This is always called')
+}
+
+
+
+Fairly similar. Slight differences.
+- 'throw' instead of 'raise'
+- 'catch' innstead of 'except'
+- different exception classes
+- also JS doesnt have the ability to have different handlers for different exceptions. Theres one handler and one needs to check the exception class to decide how to handle it.
+
+
+
+Handling exceptionns in promise code is a bit different. Instead of wrapping the promise in a try/catch block, we use a new method on the Promise class called 'catch()'. To this, a callback will be passed whenever an exception is raised anywhere along the promise chain.
+
+To add exception handling to a fetch() and JSON decoding chain, catch() needs to be called at the end: 
+
+fetch('/api/v1/posts/').then(response => {
+  return response.json()
+}).then(data  => {
+  // do something with data, for example
+  console.log(data)
+}).catch(e => {
+  console.error(e)
+})
+
+
+
+Here’s how we could add an exception on a non-200 response:
+
+fetch('/api/v1/posts/').then(response => {
+  if (response.status !== 200) {
+    throw new Error('Invalid status from server: ' + response.statusText)
+  }
+
+  return response.json()
+}).then(data  => {
+  // do something with data, for example
+  console.log(data)
+}).catch(e => {
+  console.error(e)
+})
+
+
+
+
+*/
